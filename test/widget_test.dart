f415cf1e9395e414da30b1b24f0ac3846c8d2c17
Ramtin Behrouz <ramtin.behrouz@epicalgroup.com>
2025-08-21 15:16:36 +0200

    removed unused imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:seasonal_produce/main.dart';
 
