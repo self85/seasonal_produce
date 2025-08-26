252971e6cd4d3895d7f457538adc5c3369aee527
Ramtin Behrouz <ramtin.behrouz@epicalgroup.com>
2025-08-26 20:29:49 +0200

    added b-vitamins and water

--- lib/models/produce_item.dart
+++ lib/models/produce_item.dart
@@ -6,16 +6,32 @@ class DetailedNutritionInfo {
   final double polyunsaturatedFat; // grams per 100g
   final double? sugar; // grams per 100g
   final double? starch; // grams per 100g
-  final double vitaminA; // mcg per 100g
+  final double water; // grams per 100g
+  
+  // Vitamins
+  final double vitaminA; // RE per 100g
+  final double? betaCarotene; // mcg per 100g
   final double vitaminC; // mg per 100g
   final double vitaminK; // mcg per 100g
   final double vitaminE; // mg per 100g
+  final double? vitaminD; // mcg per 100g
   final double folate; // mcg per 100g
+  final double? vitaminB6; // mg per 100g
+  final double? vitaminB12; // mcg per 100g
+  final double? thiamin; // mg per 100g (B1)
+  final double? riboflavin; // mg per 100g (B2)
+  final double? niacin; // mg per 100g (B3)
+  
+  // Minerals
   final double potassium; // mg per 100g
   final double calcium; // mg per 100g
   final double magnesium; // mg per 100g
   final double phosphorus; // mg per 100g
   final double iron; // mg per 100g
+  final double? zinc; // mg per 100g
+  final double? sodium; // mg per 100g
+  final double? selenium; // mcg per 100g
+  final double? iodine; // mcg per 100g
 
   DetailedNutritionInfo({
     required this.saturatedFat,
@@ -23,16 +39,32 @@ class DetailedNutritionInfo {
     required this.polyunsaturatedFat,
     this.sugar,
     this.starch,
+    required this.water,
+    
+    // Vitamins
     required this.vitaminA,
+    this.betaCarotene,
     required this.vitaminC,
     required this.vitaminK,
     required this.vitaminE,
+    this.vitaminD,
     required this.folate,
+    this.vitaminB6,
+    this.vitaminB12,
+    this.thiamin,
+    this.riboflavin,
+    this.niacin,
+    
+    // Minerals
     required this.potassium,
     required this.calcium,
     required this.magnesium,
     required this.phosphorus,
     required this.iron,
+    this.zinc,
+    this.sodium,
+    this.selenium,
+    this.iodine,
   });
 }
 
